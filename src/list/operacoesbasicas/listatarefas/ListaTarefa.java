3cf6be87d91f57e7eb1e1a8e6a5feea81b9c1b23
Julio Mafra <julioclmafra@gmail.com>
2024-07-14 20:16:05 -0300

    Operçaões iniciais com list

---
+++ src/list/operacoesbasicas/listatarefas/ListaTarefa.java
@@ -0,0 +1,40 @@
+package list.operacoesbasicas.listatarefas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    public void add(String descricao) {
+        this.tarefaList.add(new Tarefa(descricao));
+    }
+
+    public void remove(String descricao) {
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+
+        for (Tarefa t : this.tarefaList) {
+            if (t.getDescricao().equalsIgnoreCase(descricao)) {
+    tarefasParaRemover.add(t);
+            }
+        }
+
+        this.tarefaList.removeAll(tarefasParaRemover);
+
+    }
+
+    public int allNumberTarefas(){
+       return this.tarefaList.size();
+    }
+
+    public void getAllDescriptionTarefas()
+    {
+            System.out.println(this.tarefaList);
+    }
+
+}
+
